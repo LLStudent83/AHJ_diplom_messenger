9875397f55988e22c559ab4100ef0894df1256ea
Dmitry Prigulnov <prigulnov@mail.ru>
2022-02-24 21:48:13 +0500

    еще правлю импорт

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
 /* eslint-disable import/no-unresolved */
 import './app';
-import './modules/lazyLoading/LazyLoadingMessages';
 import './style.css';
 import './modules/popUp/popUpStyle.css';
-import './modules/popUp/PopUp';
+// import './modules/popUp/PopUp';
 import './modules/popUp/popUpAddFile.css';
 import './modules/messenger/messenger.css';
 import './modules/message/message.css';
