a3f6c68df22449d4f62ebdf645af78e73b140f8d
Dmitry Prigulnov <prigulnov@mail.ru>
2022-03-03 20:44:36 +0500

    Прописал расширения js в импорте

--- src/index.js
+++ src/index.js
@@ -1,10 +1,11 @@
+/* eslint-disable import/extensions */
 /* eslint-disable import/no-unresolved */
-import './app';
+import './app.js';
 import './style.css';
 import './modules/popUp/popUpStyle.css';
-import './modules/popUp/PopUp';
+import './modules/popUp/PopUp.js';
 import './modules/popUp/popUpAddFile.css';
 import './modules/messenger/messenger.css';
 import './modules/message/message.css';
 import './modules/popUp/popUpGps.css';
-import './service-worker';
+import './service-worker.js';
